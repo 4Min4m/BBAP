763ba29e72600000948cee0f58ab20f227d85fd6
Amin <36745993+4Min4m@users.noreply.github.com>
2024-12-30 16:33:51 +0000

    improve HTML

--- routes/protected_routes.py
+++ routes/protected_routes.py
@@ -1,12 +1,26 @@
-from flask import Blueprint, request, jsonify
-from utils.db import db  # اگر db استفاده می‌کنید
+from flask import Blueprint, request, jsonify, redirect, url_for
+from utils.db import db
+from models import Task
 
-#  Blueprint
 protected = Blueprint('protected', __name__)
 
+# Add task
 @protected.route('/tasks', methods=['POST'])
 def add_task():
     data = request.get_json()
-    if not data or 'task' not in data:
-        return jsonify({'msg': 'Invalid data'}), 400
-    return jsonify({'msg': 'Task added successfully'})
+    task_name = data.get('task')
+    new_task = Task(name=task_name)
+    db.session.add(new_task)
+    db.session.commit()
+    return jsonify({"msg": "Task added successfully!"})
+
+# Delete task
+@protected.route('/tasks/<int:id>', methods=['DELETE'])
+def delete_task(id):
+    task = Task.query.get(id)
+    if task:
+        db.session.delete(task)
+        db.session.commit()
+        return jsonify({"msg": "Task deleted successfully!"})
+    else:
+        return jsonify({"msg": "Task not found"}), 404
